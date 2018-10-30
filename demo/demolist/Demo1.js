d582780f034f32d05e1d75a50d1acedd4ed67bce
ygtzz <ygtzz@126.com>
2018-10-30 22:51:59 +0800

    修正demo1样式

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -40,7 +40,7 @@ class Demo1 extends Component {
         let {placement,showDrawer,title} = this.state;
 
         return (
-            <div className="demoPadding">
+            <div className="demoPadding demo1">
                 <div className="placementc">
                     <Radio.RadioGroup
                         name="placement"
@@ -52,7 +52,9 @@ class Demo1 extends Component {
                         <Radio value="bottom" >bottom</Radio>
                     </Radio.RadioGroup>
                 </div>
-                <button className="btn" onClick={this.fPopDrawer}>基本消息</button>
+                <div className="btnc">  
+                    <button className="btn" onClick={this.fPopDrawer}>基本消息</button>
+                </div>
                 <Drawer title={title} show={showDrawer} placement={placement} onClose={this.fCloseDrawer}>
                     <p>撑着油纸伞，独自彷徨在悠长、悠长 又寂寥的雨巷</p>
                     <p>我希望逢着 一个丁香一样地 结着愁怨的姑娘 </p>
