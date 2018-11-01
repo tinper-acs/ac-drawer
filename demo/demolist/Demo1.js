0ea763367aabba9dc1edc3be3081a39fbd14cc96
ygtzz <ygtzz@126.com>
2018-11-01 18:47:30 +0800

    抽屉添加左侧过渡效果

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -56,9 +56,11 @@ class Demo1 extends Component {
                     <button className="btn" onClick={this.fPopDrawer}>基本消息</button>
                 </div>
                 <Drawer title={title} show={showDrawer} placement={placement} onClose={this.fCloseDrawer}>
-                    <p>撑着油纸伞，独自彷徨在悠长、悠长 又寂寥的雨巷</p>
-                    <p>我希望逢着 一个丁香一样地 结着愁怨的姑娘 </p>
-                    <p>她是有 丁香一样的颜色 丁香一样的芬芳 丁香一样的忧愁 </p>
+                    <div className="con">
+                        <p>撑着油纸伞，独自彷徨在悠长、悠长 又寂寥的雨巷</p>
+                        <p>我希望逢着 一个丁香一样地 结着愁怨的姑娘 </p>
+                        <p>她是有 丁香一样的颜色 丁香一样的芬芳 丁香一样的忧愁 </p>                                                 
+                    </div>
                 </Drawer>
             </div>
         )
--- src/drawer/index.js
+++ src/drawer/index.js
@@ -22,26 +22,73 @@ class Drawer extends Component{
     constructor(props){
 		super(props);
 		this.state = {
-			showDrawer: true
+			showDrawer: true,
+			width: '0'
 		};
 		this.fMaskClick = this.fMaskClick.bind(this);
+		this.fDrawerTransitionEnd = this.fDrawerTransitionEnd.bind(this);
 	}
 	fMaskClick(){
 		const {onClose} = this.props;
 		onClose && onClose();
 	}
+	fDrawerTransitionEnd(e){
+		const {show} = this.props;
+		if(!show){
+			this.setState({
+				width: '0'
+			});
+		}
+	}
 	render(){
 		const {children,placement,show,title,hasHeader} = this.props;
-		//位置样式
+		//容器样式
+		let drawercStyle;
+		//mask样式
+		let maskStyle;
+		if(show){
+			drawercStyle = {
+				width: '100%'
+			}
+			maskStyle = {
+				opacity: 1,
+				width: '100%'
+			}
+		}
+		else{
+			drawercStyle = {
+				width: 0
+			}
+			maskStyle = {
+				opacity: 0,
+				width: 0
+			}
+		}
+		//抽屉类
 		const drawerClass = classNames('drawer',`drawer-${placement}`);
+		//根据位置获取抽屉样式
+		const translateHideMap = {
+			left: 'translateX(-100%)',
+			right: 'translateX(100%)',
+			top: 'translateY(-100%)',
+			bottom: 'translateY(100%)'
+		};
+		const translateShowMap = {
+			left: 'translateX(0)',
+			right: 'translateX(0)',
+			top: 'translateY(0)',
+			bottom: 'translateY(0)'
+		}
+		const translate = show ? translateShowMap[placement] : translateHideMap[placement];
+		//抽屉面板样式
 		const drawerStyle = {
-			display: (show ? 'block' : 'none')
+			transform: translate
 		}
 
 		return (
-			<div className="drawerc" style={drawerStyle}>
-				<div className="drawer-mask" onClick={this.fMaskClick}></div>
-				<div className={drawerClass}>
+			<div className="drawerc" style={drawercStyle}>
+				<div className="drawer-mask" style={maskStyle} onClick={this.fMaskClick}></div>
+				<div onTransitionEnd={this.fDrawerTransitionEnd} className={drawerClass} style={drawerStyle}>
 					{
 						hasHeader ?
 						(<div className="drawer-header">
