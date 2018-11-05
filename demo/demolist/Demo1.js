b0fe14a0cbcb798499bda91fc807716682a76ba5
ygtzz <ygtzz@126.com>
2018-11-05 15:29:36 +0800

    添加基本示例和位置示例

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -1,34 +1,27 @@
 /**
  *
- * @title toast
- * @description toast基本示例
+ * @title Drawer
+ * @description Drawer基本示例
  *
  */
 
 import React, { Component } from 'react';
 import Drawer from '../../src/index';
-import Radio from './Radio/index';
 
 class Demo1 extends Component {
     constructor(props){
         super(props);
         this.state = {
-            placement: 'left',
+            placement: 'right',
             showDrawer: false,
             title: 'Basic Drawer'
         };
-        this.fPositionChange = this.fPositionChange.bind(this);
         this.fPopDrawer = this.fPopDrawer.bind(this);
         this.fCloseDrawer = this.fCloseDrawer.bind(this);
     }
     fPopDrawer(){
         this.setState({
             showDrawer: true
-        })
-    }
-    fPositionChange(value){
-        this.setState({
-            placement: value
         })
     }
     fCloseDrawer(){
@@ -41,21 +34,10 @@ class Demo1 extends Component {
 
         return (
             <div className="demoPadding demo1">
-                <div className="placementc">
-                    <Radio.RadioGroup
-                        name="placement"
-                        selectedValue={placement}
-                        onChange={this.fPositionChange}>
-                        <Radio value="left" >left</Radio>
-                        <Radio value="right" >right</Radio>
-                        <Radio value="top" >top</Radio>
-                        <Radio value="bottom" >bottom</Radio>
-                    </Radio.RadioGroup>
-                </div>
                 <div className="btnc">  
-                    <button className="btn" onClick={this.fPopDrawer}>基本消息</button>
+                    <button className="btn" onClick={this.fPopDrawer}>打开</button>
                 </div>
-                <Drawer destroyOnClose={true} showClose={true} hasHeader={true} zIndex={3333} showMask={true} maskClosable={false} title={title} show={showDrawer} placement={placement} onClose={this.fCloseDrawer}>
+                <Drawer title={title} show={showDrawer} placement={placement} onClose={this.fCloseDrawer}>
                     <div className="con">
                         <p>撑着油纸伞，独自彷徨在悠长、悠长 又寂寥的雨巷</p>
                         <p>我希望逢着 一个丁香一样地 结着愁怨的姑娘 </p>
--- demo/demolist/Demo2.js
+++ demo/demolist/Demo2.js
@@ -1,44 +1,71 @@
 /**
  *
- * @title toast
- * @description toast基本示例
+ * @title Drawer
+ * @description Drawer自定义位置示例
  *
  */
 
 import React, { Component } from 'react';
 import Drawer from '../../src/index';
+import Radio from './Radio/index';
 
-class Demo2 extends Component {
+class Demo1 extends Component {
     constructor(props){
         super(props);
         this.state = {
-            placement: 'left'
+            placement: 'right',
+            showDrawer: false,
+            title: 'Drawer'
         };
+        this.fPositionChange = this.fPositionChange.bind(this);
+        this.fPopDrawer = this.fPopDrawer.bind(this);
+        this.fCloseDrawer = this.fCloseDrawer.bind(this);
     }
     fPopDrawer(){
-        
+        this.setState({
+            showDrawer: true
+        })
     }
-    changeColor(e){
-        console.log(e.target.value);
+    fPositionChange(value){
+        this.setState({
+            placement: value
+        })
+    }
+    fCloseDrawer(){
+        this.setState({
+            showDrawer: false
+        })
     }
     render () {
+        let {placement,showDrawer,title} = this.state;
+        title = placement[0].toUpperCase() + placement.slice(1) + ' ' + title;
 
         return (
-            <div className="demoPadding">
-                <div onChange={this.changeColor}>
-                    <h2>选择你喜欢的颜色</h2>    
-                    <input type="radio" id="color1" name="color" value="red" /> 
-                    <label htmlFor="color1" >red</label>
-                
-                    <input type="radio" id="color2" name="color" value="yellow"/>
-                    <label htmlFor="color2">yellow</label>
-                
-                    <input type="radio" id="color3" name="color" value="green"/>
-                    <label htmlFor="color3">green</label>
+            <div className="demoPadding demo1">
+                <div className="placementc">
+                    <Radio.RadioGroup
+                        name="placement"
+                        selectedValue={placement}
+                        onChange={this.fPositionChange}>
+                        <Radio value="left" >left</Radio>
+                        <Radio value="right" >right</Radio>
+                        <Radio value="top" >top</Radio>
+                        <Radio value="bottom" >bottom</Radio>
+                    </Radio.RadioGroup>
                 </div>
+                <div className="btnc">  
+                    <button className="btn" onClick={this.fPopDrawer}>打开</button>
+                </div>
+                <Drawer title={title} show={showDrawer} placement={placement} onClose={this.fCloseDrawer}>
+                    <div className="con">
+                        <p>撑着油纸伞，独自彷徨在悠长、悠长 又寂寥的雨巷</p>
+                        <p>我希望逢着 一个丁香一样地 结着愁怨的姑娘 </p>
+                        <p>她是有 丁香一样的颜色 丁香一样的芬芳 丁香一样的忧愁 </p>                                                 
+                    </div>
+                </Drawer>
             </div>
         )
     }
 }
 
-export default Demo2;
+export default Demo1;
